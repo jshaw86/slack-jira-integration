55ce4b8cefe0247e0e0e5acd3c80d5b1864a220e
Jordan Shaw <jordan.shaw@textnow.com>
2021-12-13 15:10:42 -0600

    slack signature calc
    Signed-off-by: Jordan Shaw <jordan.shaw@textnow.com>

--- cmd/slack-jira-integration/main.go
+++ cmd/slack-jira-integration/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/andygrunwald/go-jira"
@@ -14,11 +15,13 @@ import (
 func main() {
 	viper.BindEnv("USER_NAME")
 	viper.BindEnv("PASSWORD")
+	viper.BindEnv("SIGNING_SECRET")
 
 	username := viper.GetString("USER_NAME")
 	password := viper.GetString("PASSWORD")
+	signingSecret := viper.GetString("SIGNING_SECRET")
 
-	fmt.Println(fmt.Sprintf("username %s password %s", username, password))
+	fmt.Println(fmt.Sprintf("username %s password %s secret %s", username, password, signingSecret))
 
 	tp := jira.BasicAuthTransport{
 		Username: username,
@@ -28,7 +31,8 @@ func main() {
 	jiraClient, _ := jira.NewClient(tp.Client(), "https://jordanshaw.atlassian.net/")
 
 	r := runtime{
-		JiraClient: jiraClient,
+		JiraClient:    jiraClient,
+		SigningSecret: signingSecret,
 	}
 
 	router := mux.NewRouter()
@@ -40,11 +44,36 @@ func main() {
 }
 
 type runtime struct {
-	JiraClient *jira.Client
+	JiraClient    *jira.Client
+	SigningSecret string
+}
+
+func slackSignature(versionNumber string, slackRequestTimestamp string, body string, secret string) string {
+	return fmt.Sprintf("%s:%s:%s", versionNumber, slackRequestTimestamp, body)
+
 }
 
 func (r *runtime) SlackEventsHandler(resp http.ResponseWriter, req *http.Request) {
+	timestamp := req.Header.Get("X-Slack-Request-Timestamp")
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		resp.WriteHeader(http.StatusBadRequest)
+		resp.Write([]byte(err.Error()))
+
+	}
+	givenSlackSignature := req.Header.Get("X-Slack-Signature")
+
+	calcSlackSignature := slackSignature("v0", timestamp, string(body), r.SigningSecret)
+
+	if givenSlackSignature != calcSlackSignature {
+		fmt.Println(fmt.Errorf("calc %s given %s", calcSlackSignature, givenSlackSignature))
+		resp.WriteHeader(http.StatusBadRequest)
+		resp.Write([]byte("signatures don't match"))
+
+	}
+
 	resp.WriteHeader(http.StatusOK)
+	resp.Write([]byte("thing"))
 
 }
 
